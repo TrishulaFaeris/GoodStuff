9e8fb9943f7b3669c615c050af7b412f9c925938
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-09-12 11:46:19 -0400

    added coding conventions

--- Kevin Lin/src/intro/IntroMain.java
+++ Kevin Lin/src/intro/IntroMain.java
@@ -5,5 +5,9 @@ public class IntroMain {
 //testing for pushing
 //testing for pulling2
 	public static void main(String[] args) {
+		//declaration if a local variable
+		//notice the use of the word 'new'
+		//new must be used to call a constructor
+		CodingConventions conventionsInstance = new CodingConventions();
 	}
 }
