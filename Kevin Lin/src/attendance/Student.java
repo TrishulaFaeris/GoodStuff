d5ad66c947c21bd24912dbba0c74fb8d789611a4
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-09-26 11:38:44 -0400

    Created Student

---
+++ Kevin Lin/src/attendance/Student.java
@@ -0,0 +1,40 @@
+package attendance;
+
+public class Student implements Attendee{
+
+	public boolean isPresent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setPresent(boolean present) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getFirstName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getLastName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean mathces(String first, String last) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean matches(String last) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String getReportString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
