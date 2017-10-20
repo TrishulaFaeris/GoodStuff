c86c86564e2f0e9383f5900da51060a258e60272
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-10-20 11:46:06 -0400

    more stuff

--- Kevin Lin/src/arrays2d/Main.java
+++ Kevin Lin/src/arrays2d/Main.java
@@ -6,5 +6,30 @@ public class Main {
 		// TODO Auto-generated method stub
 
 	}
-
+	/*
+	 * This idea is another major topic in Ap CSA.
+	 * It is an idea of avoiding an ArrayIndextOutofBonds
+	 * you will see it on quizzes, tests, and projects
+	 * So always  be mindful.
+	 */
+	public static void changeNeighbors(int[] arr, int psn) {
+		if(psn >= arr.length) {
+			arr = arr; 
+		}
+		else
+		{
+			if(psn-1 == 0) {
+				arr[psn] = arr[psn]+1;
+				arr[psn+1] = arr[psn+1]-1;
+			}else if(psn == arr.length-1) {
+				arr[psn] = arr[psn]+1;
+				arr[psn-1] = arr[psn-1]-1;
+			}
+			else {
+				arr[psn] = arr[psn]+1;
+				arr[psn-1] = arr[psn-1]-1;
+				arr[psn+1] = arr[psn+1]-1;
+			}
+		}
+	}
 }
