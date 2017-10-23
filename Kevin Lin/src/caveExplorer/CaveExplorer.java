a76d7def4e1564618b5b28d835570eb8fc0a39e0
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-10-23 11:14:34 -0400

    Starting stuff

---
+++ Kevin Lin/src/caveExplorer/CaveExplorer.java
@@ -0,0 +1,13 @@
+package caveExplorer;
+
+public class CaveExplorer {
+	public static CaveRoom[][] caves;//every room in cave
+	public static Scanner in;//for user input
+	public static CaveRoom currentRoom;// changes based on how the user navigated
+	public static Inventory inventory; //where all the objects in the cave are found and kept`  ``````````
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
