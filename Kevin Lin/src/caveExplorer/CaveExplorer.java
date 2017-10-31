1c093408091a4c0a779e5577ed44176be1e4efd4
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-10-31 10:29:50 -0400

    things

--- Kevin Lin/src/caveExplorer/CaveExplorer.java
+++ Kevin Lin/src/caveExplorer/CaveExplorer.java
@@ -3,10 +3,11 @@ package caveExplorer;
 import java.util.Scanner;
 
 public class CaveExplorer {
-	public static CaveRoom[][] caves;//every room in cave
+
+	public static CaveRoom[][] caves;//every room in the cave
 	public static Scanner in;//for user input
-	public static CaveRoom currentRoom;// changes based on how the user navigated
-	public static Inventory inventory; //where all the objects in the cave are found and kept
+	public static CaveRoom currentRoom;//changes based on how the user navigated
+	public static Inventory inventory;//where all objects found in cave are kept
 	public static boolean playing = true;
 	
 	public static void main(String[] args) {
@@ -17,21 +18,20 @@ public class CaveExplorer {
 		startExploring();
 	}
 
+
 	private static void startExploring() {
 		while(playing) {
 			print(inventory.getDescription());
 			print(currentRoom.getDescription());
-			print(currentRoom.getDirections());
 			print("What would you like to do?");
 			String input = in.nextLine();
 			currentRoom.interpretInput(input);
 		}
-		
-	}
-
-	private static void print(String s) {
-		// TODO Auto-generated method stub
-		System.out.println(s);
 	}
 	
+	public static void print(String s) {
+		//NOTE: later, you can replace this line with the more sophistocated "multiLinePrint" from Chatbot
+		System.out.println(s);
+	}
+
 }
--- Kevin Lin/src/caveExplorer/Inventory.java
+++ Kevin Lin/src/caveExplorer/Inventory.java
@@ -3,6 +3,7 @@ package caveExplorer;
 public class Inventory {
 
 	private String map;
+	
 	public Inventory() {
 		updateMap();
 	}
@@ -10,46 +11,46 @@ public class Inventory {
 	public void updateMap() {
 		map = " ";
 		//create line across top:
-		for(int i = 0; i < CaveExplorer.caves[0].length - 1; i++) {
-			map+="____"; //4 underscores
+		for(int i = 0; i < CaveExplorer.caves[0].length -1; i++) {
+			map += "____";//4 underscores
 		}
-		map+= "___\n";//3 underscores, makes the corner looks nice
-		for(CaveRoom[] row: CaveExplorer.caves) {
+		map+= "___\n";//3 underscores, makes the corner look symmetrical
+		for(CaveRoom[] row : CaveExplorer.caves) {
 			//3 rows of text
-			for(int i =0; i < 3; i++) {
+			for(int i = 0; i < 3; i++) {
 				String text = "";
 				for(CaveRoom cr : row) {
 					//if door is open, leave open
 					if(cr.getDoor(CaveRoom.WEST) != null &&
-						cr.getDoor(CaveRoom.WEST).isOpen()) {
-						text +=" ";
-					}
-					else {
-						text+="|";
+							cr.getDoor(CaveRoom.WEST).isOpen()) {
+						text += " ";
+					}else {
+						text += "|";
 					}
 					//contents of room depend on what row this is
-					if(i == 0) {
-						text+= "   ";//3 spaces
+					if(i==0) {
+						text+="   ";//3 spaces
 					}else if(i == 1) {
 						text += " "+cr.getContents()+" ";
-					}
-					else if(i ==2) {
-						if(cr.getDoor(CaveRoom.SOUTH) != null &&
-								cr.getDoor(CaveRoom.SOUTH).isOpen()) {
-							text += "   ";
+					}else if(i == 2) {
+						//draw space if door to south is open
+						if(cr.getDoor(CaveRoom.SOUTH) != null && 
+						cr.getDoor(CaveRoom.SOUTH).isOpen()){
+							text+="   ";//3 spaces
 						}else {
-							text+= "___";
+							text += "___";
 						}
 					}
-					
 				}//last caveroom in row
 				text+="|";
-				map += text + "\n";
+				map += text +"\n";
 			}
 		}
 	}
+
 	public String getDescription() {
 		return map;
+//		return "You have nothing in your inventory.";
 	}
 
 }
