fa89b398f251dcc4e1e296647cd402d5a62a4f74
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-11-20 10:40:52 -0500

    smol things

--- Kevin Lin/src/javaTheHardWay/Wilderness.java
+++ Kevin Lin/src/javaTheHardWay/Wilderness.java
@@ -1,10 +1,49 @@
 package javaTheHardWay;
 
 public class Wilderness extends Habitat {
+	
+	private String description;
 
+	public Wilderness(String description, int livingCapacity) {
+		super(livingCapacity);
+		this.description = description;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Wilderness w = new Wilderness("An empty dessert",10);
+		w.stimulate(3);
+	}
+	
+	public String toString() {
+		return this.description + "\n"+ animalReport();
 	}
 
+	public void stimulate(int rounds) {
+		for(int i = 0; i < rounds; i++) {
+			for(Animal a : getAnimals()) {
+				if(a instanceof ReproductionAnimal)
+				{
+					((ReproductionAnimal)a).reset();
+				}
+			}
+			int predatorCount  = 0;
+			int preyCount = 0;
+			for(Animal a: getAnimals()) {
+			//Note: You must create a Predator and a prey class but they can be empty 
+			if(a instanceof Predator) {
+				predatorCount++;
+				}else if(a instanceof Prey) {
+					preyCount++;
+				}
+			}
+			String s = "\nRound "+(i+1)+ "\nPREDATORS: "+predatorCount+", PREY: "+preyCount+"\n" + animalReport();
+			System.out.println(s);
+			for(Animal a : getAnimals()) {
+				if(a != null) {
+					a.act();
+				}
+			}
+			growPlants();
+		}
+	}
 }
