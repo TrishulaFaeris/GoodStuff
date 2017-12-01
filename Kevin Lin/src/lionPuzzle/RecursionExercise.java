71cfc0404231a184a87efa4e4da519b630e91e28
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-12-01 11:45:32 -0500

    mec

---
+++ Kevin Lin/src/lionPuzzle/RecursionExercise.java
@@ -0,0 +1,27 @@
+package lionPuzzle;
+
+public class RecursionExercise {
+	public static void main(String[] args) {
+		int value = 5;
+		System.out.println(value+"! is equal to "+factorial(value));
+		System.out.println(value+" has "+countPrimeFactors(value,2)+" prime factors.");
+	}
+
+	private static int factorial(int value) {
+		if(value == 0) {
+			return 1;
+		}
+		else {
+			return value * factorial(value-1);
+		}
+	}
+
+	private static int countPrimeFactors(int value, int i) {
+		int test = i;
+		if(value == 1) {
+			return 0;
+		}else if(value%i ==0s){
+		
+		}
+	}
+}
